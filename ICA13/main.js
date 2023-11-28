31cea6d88397e393054949d22c22f153790c848f
grantolbricht <grol4294@colorado.edu>
2023-11-28 10:24:23 -0700

    Update main.js

--- ICA13/main.js
+++ ICA13/main.js
@@ -56,4 +56,47 @@ draw() {
     this.x += this.velX;
     this.y += this.velY;
   }
+  collisionDetect() {
+    for (const ball of balls) {
+      if (this !== ball) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+  
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
+        }
+      }
+    }
+  }
 }
+
+const balls = [];
+
+while (balls.length < 25) {
+  const size = random(30, 50);
+  const ball = new Ball(
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-7, 7),
+    random(-7, 7),
+    randomRGB(),
+    size,
+  );
+
+  balls.push(ball);
+}
+
+function loop() {
+  ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
+  ctx.fillRect(0, 0, width, height);
+
+  for (const ball of balls) {
+    ball.draw();
+    ball.update();
+  }
+
+  requestAnimationFrame(loop);
+}
+
+loop();

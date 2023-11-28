371413a5be1946a8b1aa0104afec59303963efdd
grantolbricht <grol4294@colorado.edu>
2023-11-28 10:13:53 -0700

    :}

---
+++ ICA13/main.js
@@ -0,0 +1,59 @@
+// setup canvas
+
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
+
+// function to generate random number
+
+function random(min, max) {
+  const num = Math.floor(Math.random() * (max - min + 1)) + min;
+  return num;
+}
+
+// function to generate random color
+
+function randomRGB() {
+  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+}
+class Ball {
+  constructor(x, y, velX, velY, color, size) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.size = size;
+  }
+
+draw() {
+  ctx.beginPath();
+  ctx.fillStyle = this.color;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.fill();
+  } 
+  
+  update() {
+    //this handles right edge of the screen
+    if ((this.x + this.size) >= width) {
+      this.velX = -(this.velX);
+    }
+  //this handles left side of screen
+    if ((this.x - this.size) <= 0) {
+      this.velX = -(this.velX);
+    }
+  //this handles bottom of screen
+    if ((this.y + this.size) >= height) {
+      this.velY = -(this.velY);
+    }
+  //this handles top of screen
+    if ((this.y - this.size) <= 0) {
+      this.velY = -(this.velY);
+    }
+  
+    this.x += this.velX;
+    this.y += this.velY;
+  }
+}
